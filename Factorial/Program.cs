be0d7bc50f2a21f78376db48cd08a1f8a57541cd
theac <theac@DESKTOP-LEARA2L>
2024-01-08 19:57:22 +0300

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_1_Exercise_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск метода асинхронно.");
+            Console.WriteLine("Введите значение для которого необходимо рассчитать факториал.");
+            int numForFactorial = Convert.ToInt32(Console.ReadLine());
+
+            int fact = CalcFactorialAsync(numForFactorial).Result;
+
+            Console.WriteLine(fact);
+            Console.ReadKey();
+        }
+
+        static async Task<int> CalcFactorialAsync(int num)
+        {
+            Console.WriteLine("Начинаем выполнять метод с async.");
+            int result = await Task.Run(() => CalcFactorial(num));
+            Console.WriteLine("Вычисления с await закончил.");
+            return result;
+        }
+
+        static int CalcFactorial(int num)
+        {
+            int factorial = 1;
+
+            for (int i = 2; i < num + 1; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Factorial/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Factorial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Factorial")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fd7d5b24-88b3-4f86-b987-359ed39e1b66")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
